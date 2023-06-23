644d9c6a75a44310b3828f1032dfea67406f273f
Marco Vomiero <mvomiero@student.42berlin.de>
2023-06-23 13:58:02 +0200

    initialized header files

---
+++ includes/minirt.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvomiero <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/23 13:29:11 by mvomiero          #+#    #+#             */
+/*   Updated: 2023/06/23 13:56:42 by mvomiero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+# include <X11/keysym.h> 		
+# include <X11/X.h>
+# include "../minilibx-linux/mlx.h"
+# include "../libft/libft.h"
+# include "minirt_structs.h"
+# include "Colors.h"
+# include <stdio.h>
+# include <math.h>
+
+#endif
