9b707d2cb0d49ffd626ad8ec3c29a409a38df0b0
Marco Vomiero <mvomiero@student.42berlin.de>
2023-06-23 16:52:58 +0200

    small changes main and parser

--- sources/main.c
+++ sources/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lde-ross <lde-ross@student.42berlin.de     +#+  +:+       +#+        */
+/*   By: mvomiero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 13:27:58 by mvomiero          #+#    #+#             */
-/*   Updated: 2023/06/23 16:24:55 by lde-ross         ###   ########.fr       */
+/*   Updated: 2023/06/23 16:51:48 by mvomiero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,10 @@ int	main(int argc, char *argv[])
 		ft_putendl_fd("Wrong number of arguments", 2);
 		return (1);
 	}
+	int *ptr = NULL;  // Initializing pointer to NULL
+
+    *ptr = 10;  // Trying to dereference a NULL pointer
+
+    printf("The value is: %d\n", *ptr);
 	parser(argv[1]);
 }
--- sources/parse/parser.c
+++ sources/parse/parser.c
@@ -2,7 +2,7 @@
 
 bool parser(char *arg)
 {
-	if (ft_strncmp(ft_strrchr(arg, '.'), ".rt", 3) != 0)
+	if (ft_strncmp(ft_strrchr(arg, '.'), ".rt", 4) != 0)
 	{
 		ft_putendl_fd("Error", 2);
 		ft_putendl_fd("Wrong file extension", 2);
