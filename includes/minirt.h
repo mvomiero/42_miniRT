54d6426f8390cc915da0c0c63574e60e455271a0
Marco Vomiero <mvomiero@student.42berlin.de>
2023-06-23 16:24:17 +0200

    stdbool header added

--- includes/minirt.h
+++ includes/minirt.h
@@ -6,7 +6,7 @@
 /*   By: mvomiero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 13:29:11 by mvomiero          #+#    #+#             */
-/*   Updated: 2023/06/23 13:56:42 by mvomiero         ###   ########.fr       */
+/*   Updated: 2023/06/23 16:23:59 by mvomiero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,7 @@
 # include "Colors.h"
 # include <stdio.h>
 # include <math.h>
+# include <stdbool.h>
+// expands bool to _Bool (datatype), true = 1, false = 0
 
 #endif
