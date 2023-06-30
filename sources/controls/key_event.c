c72b2c6d2e0252e9a89317e5747ffc42ffa1388c
Marco Vomiero <mvomiero@student.42berlin.de>
2023-06-30 16:42:05 +0200

    rotation cylinder implemented

--- sources/controls/key_event.c
+++ sources/controls/key_event.c
@@ -6,7 +6,7 @@
 /*   By: mvomiero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 10:47:14 by mvomiero          #+#    #+#             */
-/*   Updated: 2023/06/30 16:16:51 by mvomiero         ###   ########.fr       */
+/*   Updated: 2023/06/30 16:41:03 by mvomiero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,53 @@
 
 #define STEP 2
 #define SCALE_FACTOR 1.2
+#define ROTATION_ANGLE 20
+
+void rotate_vector_y(double angle, t_vect* vect)
+{
+	double newX = vect->x * cos(angle) + vect->z * sin(angle);
+	double newZ = -vect->x * sin(angle) + vect->z * cos(angle);
+	vect->x = newX;
+	vect->z = newZ;
+}
+
+void rotate_vector_x(double angle, t_vect* vect)
+{
+	double newY = vect->y * cos(angle) - vect->z * sin(angle);
+	double newZ = vect->y * sin(angle) + vect->z * cos(angle);
+	vect->y = newY;
+	vect->z = newZ;
+}
+
+void rotate_vector_z(double angle, t_vect* vect)
+{
+	double newX = vect->x * cos(angle) - vect->y * sin(angle);
+	double newY = vect->x * sin(angle) + vect->y * cos(angle);
+	vect->x = newX;
+	vect->y = newY;
+}
+
+
+void rotate_element(int keycode, t_vect* norm_vect)
+{
+	double angle = ROTATION_ANGLE * M_PI / 180.0; // Convert degrees to radians
+
+	if (keycode == KEY_H)
+		rotate_vector_y(angle, norm_vect); // Rotate counterclockwise along the y-axis
+	else if (keycode == KEY_J)
+		rotate_vector_y(-angle, norm_vect); // Rotate clockwise along the y-axis
+	else if (keycode == KEY_U)
+		rotate_vector_x(angle, norm_vect); // Rotate counterclockwise along the x-axis
+	else if (keycode == KEY_N)
+		rotate_vector_x(-angle, norm_vect); // Rotate clockwise along the x-axis
+	else if (keycode == KEY_Y)
+		rotate_vector_z(angle, norm_vect); // Rotate counterclockwise along the z-axis
+	else if (keycode == KEY_M)
+		rotate_vector_z(-angle, norm_vect); // Rotate clockwise along the z-axis
+}
+
+
+
 
 void move_element(int keycode, t_vect *pos)
 {
@@ -54,32 +101,15 @@ bool is_scale_key(int keycode)
 	return false;
 }
 
-/*void	transform_sphere(int keycode, t_data *data, t_type *selected_type)
+bool is_rotation_key(int keycode)
 {
-	static t_sphere *selected_sphere = NULL;
+	if (keycode == KEY_H || keycode == KEY_J || keycode == KEY_U ||
+		keycode == KEY_N || keycode == KEY_Y || keycode == KEY_M)
+		return true;
+	return false;
+}
 
-	if (keycode == KEY_S || is_movement_key(keycode) || is_scale_key(keycode))
-	{
-		if (*selected_type != TYPE_SPHERE && keycode == KEY_S)
-		{
-			*selected_type = TYPE_SPHERE;
-			selected_sphere = data->spheres;
-		}
-		else if (*selected_type == TYPE_SPHERE && keycode == KEY_S)
-		{
-			selected_sphere = selected_sphere->next;
-			if (selected_sphere == NULL)
-				selected_sphere = data->spheres;
-		}
-		if (selected_sphere != NULL)
-		{
-			if (is_movement_key(keycode))
-				move_element(keycode, &(selected_sphere->pos));
-			if (is_scale_key(keycode))
-				scale_element(keycode, &(selected_sphere->diameter));
-		}
-	}
-}*/
+
 void	transform_sphere(int keycode, t_data *data, t_type *selected_type)
 {
 	static t_sphere *selected_sphere = NULL;
@@ -134,6 +164,8 @@ void transform_cylinder(int keycode, t_data* data, t_type* selected_type)
 			scale_element(keycode, &(selected_cylinder->diameter));
 			scale_element(keycode, &(selected_cylinder->height));
 		}
+		if (is_rotation_key(keycode))
+			rotate_element(keycode, &(selected_cylinder->norm_vect));
 	}
 }
 
