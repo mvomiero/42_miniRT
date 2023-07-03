2e11331ed0db120222b1440feb3e53cef4f3fd4d
Marco Vomiero <mvomiero@student.42berlin.de>
2023-07-03 12:42:22 +0200

    cylinder works,but not top and bottom disks

--- sources/render/intersect_cylinder.c
+++ sources/render/intersect_cylinder.c
@@ -6,7 +6,7 @@
 /*   By: mvomiero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 15:15:06 by mvomiero          #+#    #+#             */
-/*   Updated: 2023/07/03 12:20:54 by mvomiero         ###   ########.fr       */
+/*   Updated: 2023/07/03 12:42:08 by mvomiero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,66 +14,7 @@
 
 #include <math.h>
 
-bool intersectPlane(const t_vect *n, const t_vect *p0, const t_vect *l0, const t_vect *l, double *t)
-{
-    double denom = vectorDotProduct(*n, *l);
-
-    if (fabs(denom) < EPSILON)
-        return false;
-
-    *t = vectorDotProduct(*n, vectorSubtraction(*p0, *l0)) / denom;
-
-    if (*t >= 0)
-        return true;
-
-    return false;
-}
-
-bool intersectDisk(const t_vect *n, const t_vect *p0, const double *radius, const t_vect *l0, const t_vect *l)
-{
-    double t = 0;
-    if (intersectPlane(n, p0, l0, l, &t)) {
-        t_vect p = vectorAdd(*l0, vectorScale(*l, t));
-        t_vect v = vectorSubtraction(p, *p0);
-        double d2 = vectorDotProduct(v, v);
-        return (sqrt(d2) <= *radius);
-    }
-
-    return false;
-}
-
-
-
 int check_cy(t_cylinder *cy, t_vect hpnt)
-{
-    t_vect pmin = vectorAdd(cy->pos, vectorScale(cy->norm_vect, -0.5 * cy->height));
-    t_vect pmax = vectorAdd(cy->pos, vectorScale(cy->norm_vect, 0.5 * cy->height));
-
-    if (vectorDotProduct(cy->norm_vect, vectorSubtraction(hpnt, pmin)) > 0 && vectorDotProduct(cy->norm_vect, vectorSubtraction(hpnt, pmax)) < 0)
-        return 1;
-
-    double radius2 = cy->diameter / 2 * cy->diameter / 2;
-
-    t_vect disk_center = vectorAdd(cy->pos, vectorScale(cy->norm_vect, -0.5 * cy->height));
-    t_vect scaled_norm_vect = vectorScale(cy->norm_vect, -1.0);
-    if (intersectDisk(&cy->norm_vect, &disk_center, &radius2, &hpnt, &scaled_norm_vect))
-        return 1;
-
-    disk_center = vectorAdd(cy->pos, vectorScale(cy->norm_vect, 0.5 * cy->height));
-    if (intersectDisk(&cy->norm_vect, &disk_center, &radius2, &hpnt, &cy->norm_vect))
-        return 1;
-
-    return 0;
-}
-
-
-
-
-
-
-
-
-/*int check_cy(t_cylinder *cy, t_vect hpnt)
 {
 	t_vect pmax;
 	t_vect pmin;
@@ -96,7 +37,7 @@ int check_cy(t_cylinder *cy, t_vect hpnt)
 		return 1;
 
 	return 0;
-}*/
+}
 
 void hit_cylinder(t_data *data, t_cylinder *cylinders, t_vect rayOrigin, t_vect rayDirection)
 {
@@ -162,76 +103,3 @@ void hit_cylinder(t_data *data, t_cylinder *cylinders, t_vect rayOrigin, t_vect 
 	}
 }
 
-
-/*void hit_cylinder(t_data *data, t_cylinder *cylinders, t_vect rayOrigin, t_vect rayDirection)
-{
-	while (cylinders)
-	{
-		// Calculate the direction vector of the cylinder's axis
-		t_vect axisDirection = cylinders->norm_vect;
-
-		// Calculate the vector from the ray origin to the cylinder's position
-		t_vect oc = {rayOrigin.x - cylinders->pos.x, rayOrigin.y - cylinders->pos.y, rayOrigin.z - cylinders->pos.z};
-
-		// Calculate the dot products needed for the quadratic equation
-		double a = vectorDotProduct(rayDirection, rayDirection) - pow(vectorDotProduct(rayDirection, axisDirection), 2);
-		double b = 2 * (vectorDotProduct(rayDirection, oc) - vectorDotProduct(rayDirection, axisDirection) * vectorDotProduct(oc, axisDirection));
-		double c = vectorDotProduct(oc, oc) - pow(vectorDotProduct(oc, axisDirection), 2) - pow(cylinders->diameter / 2, 2);
-
-		// Solve the quadratic equation to find the intersection points
-		double discriminant = b * b - 4 * a * c;
-
-		if (discriminant >= 0)
-		{
-			// Calculate the solutions
-			// Quadratic equations always have two solutions, one with -sqrt and the other with +sqrt
-			double t1 = (-b - sqrt(discriminant)) / (2 * a);
-			double t2 = (-b + sqrt(discriminant)) / (2 * a);
-
-			// Check if the solutions are within the valid range and closer than the current closest hit
-			// > 0 means that they are not in the ray but behind the origin
-			if (t1 > 0 && t1 < data->pix.t)
-			{
-				// Calculate the y-coordinate of the intersection point
-				//double y = rayOrigin.y + t1 * rayDirection.y;
-				
-				// Calculate the intersection point
-				t_vect hitpoint = vectorAdd(rayOrigin, vectorScale(rayDirection, t1));
-
-				// Calculate the y-coordinate of the intersection point
-				double y = hitpoint.y;
-
-				// Check if the intersection point is within the height of the cylinder
-				if (y >= cylinders->pos.y && y <= cylinders->pos.y + cylinders->height)
-				{
-					data->pix.t = t1;
-					data->pix.color = cylinders->color;
-					data->pix.hitpoint = vectorAdd(rayOrigin, vectorScale(rayDirection, t1));
-					data->pix.normal = vectorNormalize(vectorSubtraction(data->pix.hitpoint, vectorAdd(cylinders->pos, vectorScale(axisDirection, vectorDotProduct(vectorSubtraction(data->pix.hitpoint, cylinders->pos), axisDirection)))));
-				}
-					// Fill other values of pix
-			}
-
-			if (t2 > 0 && t2 < data->pix.t)
-			{
-				// Calculate the intersection point
-				t_vect hitpoint = vectorAdd(rayOrigin, vectorScale(rayDirection, t2));
-
-				// Calculate the y-coordinate of the intersection point
-				double y = hitpoint.y;
-
-				// Check if the intersection point is within the height of the cylinder
-				if (y >= cylinders->pos.y && y <= cylinders->pos.y + cylinders->height)
-				{
-					data->pix.t = t2;
-					data->pix.color = cylinders->color;
-					data->pix.hitpoint = vectorAdd(rayOrigin, vectorScale(rayDirection, t2));
-					data->pix.normal = (vectorSubtraction(data->pix.hitpoint, vectorAdd(cylinders->pos, vectorScale(axisDirection, t2))));
-					// Fill other values of pix
-				}
-			}
-		}
-
-		cylinders = cylinders->next;
-	}
-}*/
