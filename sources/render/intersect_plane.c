be988bf238b1b593e99e4304775afeb8074ba0ee
Marco Vomiero <mvomiero@student.42berlin.de>
2023-07-04 11:35:22 +0200

    disks are visible

--- sources/render/intersect_plane.c
+++ sources/render/intersect_plane.c
@@ -6,7 +6,7 @@
 /*   By: mvomiero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 16:49:58 by mvomiero          #+#    #+#             */
-/*   Updated: 2023/07/04 10:59:04 by mvomiero         ###   ########.fr       */
+/*   Updated: 2023/07/04 11:34:07 by mvomiero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,27 +50,87 @@ double vector_magnitude(t_vect vector)
     return sqrt(vector.x * vector.x + vector.y * vector.y + vector.z * vector.z);
 }
 
+t_vect move_point_along_normal(t_vect point, t_vect normal, double distance)
+{
+    // Normalize the normal vector (if it's not already normalized)
+    double magnitude = vector_magnitude(normal);
+    t_vect normalized_normal = {
+        .x = normal.x / magnitude,
+        .y = normal.y / magnitude,
+        .z = normal.z / magnitude
+    };
+    
+    // Calculate the displacement vector
+    t_vect displacement = {
+        .x = normalized_normal.x * distance,
+        .y = normalized_normal.y * distance,
+        .z = normalized_normal.z * distance
+    };
+    
+    // Move the point along the normal direction
+    t_vect moved_point = {
+        .x = point.x + displacement.x,
+        .y = point.y + displacement.y,
+        .z = point.z + displacement.z
+    };
+    
+    return moved_point;
+}
+
+t_vect get_opposite_normal(t_vect normal)
+{
+    t_vect opposite_normal = {
+        .x = -normal.x,
+        .y = -normal.y,
+        .z = -normal.z
+    };
+    
+    return opposite_normal;
+}
+
 
 bool is_cylinder_disk_hit(t_cylinder* cylinder, t_vect ray_origin, t_vect ray_direction, double* t)
 {
     double dot_product_nd = vector_dot_product(cylinder->norm_vect, ray_direction);
     
+	t_vect pos_top = move_point_along_normal(cylinder->pos, cylinder->norm_vect, cylinder->diameter/2);
     // Check if the ray is not parallel to the cylinder (dot_product_nd is close to zero)
     if (fabs(dot_product_nd) > EPSILON)
     {
-        t_vect oc = vector_substract(cylinder->pos, ray_origin);
+        t_vect oc = vector_substract(pos_top, ray_origin);
         *t = vector_dot_product(oc, cylinder->norm_vect) / dot_product_nd;
         
         if (*t > 0)
         {
             t_vect hit_point = vector_add(ray_origin, vector_scale(ray_direction, *t));
-            double distance = vector_magnitude(vector_substract(hit_point, cylinder->pos));
+            double distance = vector_magnitude(vector_substract(hit_point, pos_top));
             
             if (distance <= cylinder->diameter / 2)
                 return true;
         }
     }
-    
+
+	
+	t_vect	normal_bottom = get_opposite_normal(cylinder->norm_vect);
+	t_vect pos_bottom = move_point_along_normal(cylinder->pos, normal_bottom, cylinder->diameter/2);
+	dot_product_nd = vector_dot_product(normal_bottom, ray_direction);
+    if (fabs(dot_product_nd) > EPSILON)
+    {
+        t_vect oc = vector_substract(pos_bottom, ray_origin);
+        *t = vector_dot_product(oc, normal_bottom) / dot_product_nd;
+        
+        if (*t > 0)
+        {
+            t_vect hit_point = vector_add(ray_origin, vector_scale(ray_direction, *t));
+            double distance = vector_magnitude(vector_substract(hit_point, pos_bottom));
+            
+            if (distance <= cylinder->diameter / 2)
+                return true;
+        }
+    }
+
+	
+	
     return false;
 }
 
