62d47ff31d7ecc779bb9a3e680aa3b4fb509206b
Luca Fischer <lucafischer@fastmail.com>
2023-07-07 17:23:23 +0200

    norm

--- sources/render/intersect_cylinder.c
+++ sources/render/intersect_cylinder.c
@@ -6,7 +6,7 @@
 /*   By: lde-ross <lde-ross@student.42berlin.de     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 15:15:06 by mvomiero          #+#    #+#             */
-/*   Updated: 2023/07/07 15:48:48 by lde-ross         ###   ########.fr       */
+/*   Updated: 2023/07/07 17:22:56 by lde-ross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,68 +26,100 @@ static bool	check_cy(t_cylinder *cy, t_vect hpnt)
 	capmin = vector_dot_product(hpnt_pmin, hpnt_pmin);
 	capmax = vector_dot_product(hpnt_pmax, hpnt_pmax);
 	if (vector_dot_product(cy->norm_vect, hpnt_pmin) > 0
-			&& vector_dot_product(cy->norm_vect, hpnt_pmax) < 0)
+		&& vector_dot_product(cy->norm_vect, hpnt_pmax) < 0)
 		return (true);
-	if (capmin < pow(cy->diameter / 2, 2)
-			|| capmax < pow(cy->diameter / 2, 2))
+	if (capmin < pow(cy->diameter / 2, 2) || capmax < pow(cy->diameter / 2, 2))
 		return (true);
 	return (false);
 }
 
-static double	get_discriminat(double a, double b, t_vect	axis_direction, t_vect oc)
+static double	get_cyl_discr(t_cylinder *cyl, t_vect ray_o, t_vect ray_d)
 {
-	double	discriminant;
-
-	
+	cyl->inter.oc = vector_subtract(ray_o, cyl->pos);
+	cyl->inter.a = vector_dot_product(ray_d, ray_d)
+		- pow(vector_dot_product(ray_d, cyl->norm_vect), 2);
+	cyl->inter.b = 2 * (vector_dot_product(ray_d, cyl->inter.oc)
+			- vector_dot_product(ray_d, cyl->norm_vect)
+			* vector_dot_product(cyl->inter.oc, cyl->norm_vect));
+	cyl->inter.c = vector_dot_product(cyl->inter.oc, cyl->inter.oc)
+		- pow(vector_dot_product(cyl->inter.oc, cyl->norm_vect), 2)
+		- pow(cyl->diameter / 2, 2);
+	return (cyl->inter.b * cyl->inter.b - 4 * cyl->inter.a * cyl->inter.c);
 }
 
-bool	is_cylinder_hit(t_cylinder *cylinder, t_vect ray_origin, t_vect ray_direction, double *t, t_data *data)
+static void	fill_normal_t1(double t1, t_data *data, t_cylinder *cyl, t_vect hp)
 {
-	t_vect	axis_direction;
-	t_vect	oc;
-	double	a;
-	double	b;
-	double	c;
-	double	discriminant;
-	double	t1;
-	double	t2;
+	if (data)
+	{
+		if (t1 < data->pix.t)
+			data->pix.normal = vector_normalize(
+					vector_subtract(hp,
+						vector_add(cyl->pos,
+							vector_scale(cyl->norm_vect,
+								vector_dot_product(
+									vector_subtract(hp, cyl->pos),
+									cyl->norm_vect)))));
+	}
+}
+
+static void	fill_normal_t2(double t2, t_data *data, t_cylinder *cyl, t_vect hp)
+{
+	if (data)
+	{
+		if (t2 < data->pix.t)
+			data->pix.normal = vector_normalize(
+					vector_subtract(hp,
+						vector_add(cyl->pos,
+							vector_scale(cyl->norm_vect, t2))));
+	}
+}
+
+bool	t1_routine(t_vect ray_o, t_vect ray_d, t_cylinder *cyl, t_data *data)
+{
 	t_vect	hitpoint;
 
-	axis_direction = cylinder->norm_vect;
-	oc = vector_subtract(ray_origin, cylinder->pos);
-	a = vector_dot_product(ray_direction, ray_direction) - pow(vector_dot_product(ray_direction, axis_direction), 2);
-	b = 2 * (vector_dot_product(ray_direction, oc) - vector_dot_product(ray_direction, axis_direction) * vector_dot_product(oc, axis_direction));
-	c = vector_dot_product(oc, oc) - pow(vector_dot_product(oc, axis_direction), 2) - pow(cylinder->diameter / 2, 2);
-	discriminant = b * b - 4 * a * c;
-	if (discriminant >= 0)
+	hitpoint = vector_add(ray_o, vector_scale(ray_d, cyl->inter.t1));
+	if (check_cy(cyl, hitpoint))
 	{
-		t1 = (-b - sqrt(discriminant)) / (2 * a);
-		t2 = (-b + sqrt(discriminant)) / (2 * a);
-		if (t1 > 0 && t1 < t2)
+		fill_normal_t1(cyl->inter.t1, data, cyl, hitpoint);
+		return (true);
+	}
+	return (false);
+}
+
+bool	t2_routine(t_vect ray_o, t_vect ray_d, t_cylinder *cyl, t_data *data)
+{
+	t_vect	hitpoint;
+
+	hitpoint = vector_add(ray_o, vector_scale(ray_d, cyl->inter.t1));
+	if (check_cy(cyl, hitpoint))
+	{
+		fill_normal_t2(cyl->inter.t2, data, cyl, hitpoint);
+		return (true);
+	}
+	return (false);
+}
+
+bool	is_cylinder_hit(t_cylinder *cyl, t_vect ray_o, t_vect ray_d, t_data *data)
+{
+	cyl->inter.discriminant = get_cyl_discr(cyl, ray_o, ray_d);
+	if (cyl->inter.discriminant >= 0)
+	{
+		cyl->inter.t1 = (-cyl->inter.b - sqrt(cyl->inter.discriminant)) / (2 * cyl->inter.a);
+		cyl->inter.t2 = (-cyl->inter.b + sqrt(cyl->inter.discriminant)) / (2 * cyl->inter.a);
+		if (cyl->inter.t1 > 0 && cyl->inter.t1 < cyl->inter.t2)
 		{
-			hitpoint = vector_add(ray_origin, vector_scale(ray_direction, t1));
-			if (check_cy(cylinder, hitpoint))
+			if (t1_routine(ray_o, ray_d, cyl, data))
 			{
-				*t = t1;
-				if (data)
-				{
-					if (t1 < data->pix.t)
-						data->pix.normal = vector_normalize(vector_subtract(hitpoint, vector_add(cylinder->pos, vector_scale(axis_direction, vector_dot_product(vector_subtract(hitpoint, cylinder->pos), axis_direction)))));
-				}
+				cyl->inter.t = cyl->inter.t1;
 				return (true);
 			}
 		}
-		if (t2 > 0 && t2 < t1)
+		if (cyl->inter.t2 > 0 && cyl->inter.t2 < cyl->inter.t1)
 		{
-			hitpoint = vector_add(ray_origin, vector_scale(ray_direction, t1));
-			if (check_cy(cylinder, hitpoint))
+			if (t2_routine(ray_o, ray_d, cyl, data))
 			{
-				*t = t2;
-				if (data)
-				{
-					if (t2 < data->pix.t)
-						data->pix.normal = vector_normalize(vector_subtract(hitpoint, vector_add(cylinder->pos, vector_scale(axis_direction, t2))));
-				}
+				cyl->inter.t = cyl->inter.t2;
 				return (true);
 			}
 		}
@@ -95,15 +127,13 @@ bool	is_cylinder_hit(t_cylinder *cylinder, t_vect ray_origin, t_vect ray_directi
 	return (false);
 }
 
-void	hit_cylinder(t_data *data, t_cylinder *cylinders, t_vect ray_origin, t_vect ray_direction)
+void hit_cylinder(t_data *data, t_cylinder *cylinders, t_vect ray_origin, t_vect ray_direction)
 {
-	double	t;
-
 	while (cylinders)
 	{
-		if (is_cylinder_hit(cylinders, ray_origin, ray_direction, &t, data) && t < data->pix.t)
+		if (is_cylinder_hit(cylinders, ray_origin, ray_direction, data) && cylinders->inter.t < data->pix.t)
 		{
-			data->pix.t = t;
+			data->pix.t = cylinders->inter.t;
 			data->pix.color = cylinders->color;
 			data->pix.hitpoint = vector_add(ray_origin, vector_scale(ray_direction, t));
 		}
