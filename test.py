8f32cf9168c451731421d74050ec084251185ef1
Marco Vomiero <mvomiero@student.42berlin.de>
2023-07-17 14:06:09 +0200

    script updated

--- test.py
+++ test.py
@@ -32,192 +32,176 @@ def substitute_last_line(file_path, new_line):
 			file.writelines(lines)
 			file.truncate()
 
-# Usage example
 file_path = 'scenes/simple_scenes/camera_test.rt'
-
-
 
 print_text("\n\tMINIRT TESTS\n", BOLD, PURPLE)
 
-""" WRONG INPUT """
-print_text("\nWrong input tests:\n", ITAL, BOLD, PURPLE)
+# managing input:
+user_input = input("Enter 'parsing' for parsing tests")
 
-print_text("\nWrong text input: (blabla)", ITAL, ORANGE)
-subprocess.Popen(["./miniRT", "blabla"])
-time.sleep(TIME_WR_INPUT)
+if user_input == 'parsing':
 
-print_text("\nNo input", ITAL, ORANGE)
-subprocess.Popen(["./miniRT", ""])
-time.sleep(TIME_WR_INPUT)
+	""" WRONG INPUT """
+	print_text("\nWrong input tests:\n", ITAL, BOLD, PURPLE)
 
-print_text("\nNumeric input", ITAL, ORANGE)
-subprocess.Popen(["./miniRT", ""])
-time.sleep(TIME_WR_INPUT)
+	print_text("\nWrong text input: (blabla)", ITAL, ORANGE)
+	subprocess.Popen(["./miniRT", "blabla"])
+	time.sleep(TIME_WR_INPUT)
 
-print_text("\nNo arguments", ITAL, ORANGE)
-subprocess.Popen(["./miniRT"])
-time.sleep(TIME_WR_INPUT)
+	print_text("\nNo input", ITAL, ORANGE)
+	subprocess.Popen(["./miniRT", ""])
+	time.sleep(TIME_WR_INPUT)
 
-print_text("\nToo many arguments", ITAL, ORANGE)
-subprocess.Popen(["./miniRT", "ciao", "bello"])
-time.sleep(TIME_WR_INPUT)
+	print_text("\nNumeric input", ITAL, ORANGE)
+	subprocess.Popen(["./miniRT", ""])
+	time.sleep(TIME_WR_INPUT)
 
-print_text("\nWrong file no access", ITAL, ORANGE)
-# set permissions to 000
-os.chmod("scenes/simple_scenes/test_wrong_no_access.rt", 0o000)
-subprocess.Popen(["cat", "scenes/simple_scenes/test_wrong_no_access.rt"])
-time.sleep(TIME_PAUSE)
-subprocess.Popen(["./miniRT", "scenes/simple_scenes/test_wrong_no_access.rt"])
-time.sleep(TIME_WR_INPUT)
-os.chmod("scenes/simple_scenes/test_wrong_no_access.rt", 0o777)
+	print_text("\nNo arguments", ITAL, ORANGE)
+	subprocess.Popen(["./miniRT"])
+	time.sleep(TIME_WR_INPUT)
 
-# handling a copy of the file
-shutil.copy("scenes/simple_scenes/test_wrong.rt", "scenes/simple_scenes/test_wrong_temp.rt")
+	print_text("\nToo many arguments", ITAL, ORANGE)
+	subprocess.Popen(["./miniRT", "ciao", "bello"])
+	time.sleep(TIME_WR_INPUT)
 
-print_text("\nToo few elements", ITAL, ORANGE)
-subprocess.Popen(["./miniRT", "scenes/simple_scenes/test_wrong_temp.rt"])
-time.sleep(TIME_WR_INPUT)
+	print_text("\nWrong file no access", ITAL, ORANGE)
+	# set permissions to 000
+	os.chmod("scenes/simple_scenes/test_wrong_no_access.rt", 0o000)
+	subprocess.Popen(["cat", "scenes/simple_scenes/test_wrong_no_access.rt"])
+	time.sleep(TIME_PAUSE)
+	subprocess.Popen(["./miniRT", "scenes/simple_scenes/test_wrong_no_access.rt"])
+	time.sleep(TIME_WR_INPUT)
+	os.chmod("scenes/simple_scenes/test_wrong_no_access.rt", 0o777)
 
-print_text("\nWrong file line", ITAL, ORANGE)
-substitute_last_line("scenes/simple_scenes/test_wrong_temp.rt", "L 6,20,-20 0.7 255,255,255\nblabla")
-subprocess.Popen(["./miniRT", "scenes/simple_scenes/test_wrong_temp.rt"])
-time.sleep(TIME_WR_INPUT)
+	# handling a copy of the file
+	shutil.copy("scenes/simple_scenes/test_wrong.rt", "scenes/simple_scenes/test_wrong_temp.rt")
 
-print_text("\nToo many elements", ITAL, ORANGE)
-substitute_last_line("scenes/simple_scenes/test_wrong_temp.rt", "L 6,20,-20 0.7 255,255,255")
-subprocess.Popen(["./miniRT", "scenes/simple_scenes/test_wrong_temp.rt"])
-time.sleep(TIME_WR_INPUT)
-# delete the copied file
-os.remove("scenes/simple_scenes/test_wrong_temp.rt")
+	print_text("\nToo few elements", ITAL, ORANGE)
+	subprocess.Popen(["./miniRT", "scenes/simple_scenes/test_wrong_temp.rt"])
+	time.sleep(TIME_WR_INPUT)
 
-""" RENDERING AND CAMERA """
-print_text("\nRendering and camera tests:\n", ITAL, BOLD, PURPLE)
+	print_text("\nWrong file line", ITAL, ORANGE)
+	substitute_last_line("scenes/simple_scenes/test_wrong_temp.rt", "L 6,20,-20 0.7 255,255,255\nblabla")
+	subprocess.Popen(["./miniRT", "scenes/simple_scenes/test_wrong_temp.rt"])
+	time.sleep(TIME_WR_INPUT)
+
+	print_text("\nToo many elements", ITAL, ORANGE)
+	substitute_last_line("scenes/simple_scenes/test_wrong_temp.rt", "L 6,20,-20 0.7 255,255,255")
+	subprocess.Popen(["./miniRT", "scenes/simple_scenes/test_wrong_temp.rt"])
+	time.sleep(TIME_WR_INPUT)
+	# delete the copied file
+	os.remove("scenes/simple_scenes/test_wrong_temp.rt")
+
+else:
+
+	""" RENDERING AND CAMERA """
+	print_text("\nRendering and camera tests:\n", ITAL, BOLD, PURPLE)
 
 
-""" SPHERE """
+	""" SPHERE """
 
-print_text("\nSphere:\n", ITAL, GREEN)
+	print_text("\nSphere:\n", ITAL, GREEN)
 
-# Execute the command
-subprocess.Popen(["./miniRT", "scenes/simple_scenes/sphere.rt"])
+	# Execute the command
+	subprocess.Popen(["./miniRT", "scenes/simple_scenes/sphere.rt"])
 
-# Wait for x seconds
-time.sleep(TIME)
+	# Wait for x seconds
+	time.sleep(TIME)
 
-# Simulate pressing the Esc key
-pyautogui.press('esc')
-time.sleep(TIME_PAUSE)
+	# Simulate pressing the Esc key
+	pyautogui.press('esc')
+	time.sleep(TIME_PAUSE)
 
-""" PLANE """
+	""" PLANE """
 
-print_text("\nPlane:\n", ITAL, GREEN)
+	print_text("\nPlane:\n", ITAL, GREEN)
 
-subprocess.Popen(["./miniRT", "scenes/simple_scenes/plane.rt"])
-time.sleep(TIME)
-pyautogui.press('esc')
-time.sleep(TIME_PAUSE)
+	subprocess.Popen(["./miniRT", "scenes/simple_scenes/plane.rt"])
+	time.sleep(TIME)
+	pyautogui.press('esc')
+	time.sleep(TIME_PAUSE)
 
-""" PLANE """
+	""" PLANE """
 
-print_text("\nCylinder:\n", ITAL, GREEN)
+	print_text("\nCylinder:\n", ITAL, GREEN)
 
-subprocess.Popen(["./miniRT", "scenes/simple_scenes/cylinder.rt"])
-time.sleep(TIME)
-pyautogui.press('esc')
-time.sleep(TIME_PAUSE)
+	subprocess.Popen(["./miniRT", "scenes/simple_scenes/cylinder.rt"])
+	time.sleep(TIME)
+	pyautogui.press('esc')
+	time.sleep(TIME_PAUSE)
 
-""" CAMERA """
-#substitute_last_line(file_path, new_line)
+	""" CAMERA """
+	#substitute_last_line(file_path, new_line)
 
-print_text("\nCamera:\n", ITAL, GREEN)
+	print_text("\nCamera:\n", ITAL, GREEN)
 
-subprocess.Popen(["./miniRT", "scenes/simple_scenes/camera_test.rt"])
-time.sleep(TIME)
-pyautogui.press('esc')
-time.sleep(TIME_PAUSE)
+	subprocess.Popen(["./miniRT", "scenes/simple_scenes/camera_test.rt"])
+	time.sleep(TIME)
+	pyautogui.press('esc')
+	time.sleep(TIME_PAUSE)
 
-""" CAMERA Z front"""
-substitute_last_line(file_path, "C 0,0,0 0,0,1 55")
+	""" CAMERA Z front"""
+	substitute_last_line(file_path, "C 0,0,0 0,0,1 55")
 
-print_text("\nCamera z front:\n", ITAL, GREEN)
+	print_text("\nCamera z front:\n", ITAL, GREEN)
 
-subprocess.Popen(["./miniRT", "scenes/simple_scenes/camera_test.rt"])
-time.sleep(TIME)
-pyautogui.press('esc')
-time.sleep(TIME_PAUSE)
+	subprocess.Popen(["./miniRT", "scenes/simple_scenes/camera_test.rt"])
+	time.sleep(TIME)
+	pyautogui.press('esc')
+	time.sleep(TIME_PAUSE)
 
-""" CAMERA Z back"""
-substitute_last_line(file_path, "C 0,0,0 0,0,-1 55")
+	""" CAMERA Z back"""
+	substitute_last_line(file_path, "C 0,0,0 0,0,-1 55")
 
-print_text("\nCamera z back:\n", ITAL, GREEN)
+	print_text("\nCamera z back:\n", ITAL, GREEN)
 
-subprocess.Popen(["./miniRT", "scenes/simple_scenes/camera_test.rt"])
-time.sleep(TIME)
-pyautogui.press('esc')
-time.sleep(TIME_PAUSE)
+	subprocess.Popen(["./miniRT", "scenes/simple_scenes/camera_test.rt"])
+	time.sleep(TIME)
+	pyautogui.press('esc')
+	time.sleep(TIME_PAUSE)
 
-""" CAMERA X front"""
-substitute_last_line(file_path, "C 0,0,0 1,0,0 55")
+	""" CAMERA X front"""
+	substitute_last_line(file_path, "C 0,0,0 1,0,0 55")
 
-print_text("\nCamera x front:\n", ITAL, GREEN)
+	print_text("\nCamera x front:\n", ITAL, GREEN)
 
-subprocess.Popen(["./miniRT", "scenes/simple_scenes/camera_test.rt"])
-time.sleep(TIME)
-pyautogui.press('esc')
-time.sleep(TIME_PAUSE)
+	subprocess.Popen(["./miniRT", "scenes/simple_scenes/camera_test.rt"])
+	time.sleep(TIME)
+	pyautogui.press('esc')
+	time.sleep(TIME_PAUSE)
 
-""" CAMERA X back"""
-substitute_last_line(file_path, "C 0,0,0 -1,0,0 55")
+	""" CAMERA X back"""
+	substitute_last_line(file_path, "C 0,0,0 -1,0,0 55")
 
-print_text("\nCamera x back:\n", ITAL, GREEN)
+	print_text("\nCamera x back:\n", ITAL, GREEN)
 
-subprocess.Popen(["./miniRT", "scenes/simple_scenes/camera_test.rt"])
-time.sleep(TIME)
-pyautogui.press('esc')
-time.sleep(TIME_PAUSE)
+	subprocess.Popen(["./miniRT", "scenes/simple_scenes/camera_test.rt"])
+	time.sleep(TIME)
+	pyautogui.press('esc')
+	time.sleep(TIME_PAUSE)
 
-""" CAMERA Y up"""
-substitute_last_line(file_path, "C 0,0,0 1,0,0 55")
+	""" CAMERA Y up"""
+	substitute_last_line(file_path, "C 0,0,0 1,0,0 55")
 
-print_text("\nCamera y up:\n", ITAL, GREEN)
+	print_text("\nCamera y up:\n", ITAL, GREEN)
 
-subprocess.Popen(["./miniRT", "scenes/simple_scenes/camera_test.rt"])
-time.sleep(TIME)
-pyautogui.press('esc')
-time.sleep(TIME_PAUSE)
+	subprocess.Popen(["./miniRT", "scenes/simple_scenes/camera_test.rt"])
+	time.sleep(TIME)
+	pyautogui.press('esc')
+	time.sleep(TIME_PAUSE)
 
-""" CAMERA Y down"""
-substitute_last_line(file_path, "C 0,0,0 -1,0,0 55")
+	""" CAMERA Y down"""
+	substitute_last_line(file_path, "C 0,0,0 -1,0,0 55")
 
-print_text("\nCamera y down:\n", ITAL, GREEN)
+	print_text("\nCamera y down:\n", ITAL, GREEN)
 
-subprocess.Popen(["./miniRT", "scenes/simple_scenes/camera_test.rt"])
-time.sleep(TIME)
-pyautogui.press('esc')
-time.sleep(TIME_PAUSE)
+	subprocess.Popen(["./miniRT", "scenes/simple_scenes/camera_test.rt"])
+	time.sleep(TIME)
+	pyautogui.press('esc')
+	time.sleep(TIME_PAUSE)
 
-#reset file to initial status
-substitute_last_line(file_path, "C 4,10,-70 -0.2,0,0.7 90")
+	#reset file to initial status
+	substitute_last_line(file_path, "C 4,10,-70 -0.2,0,0.7 90")
 
-print_text("\n\t🌞 DONE!!! 🌞\n", ITAL, BOLD, PURPLE)
+	print_text("\n\t🌞 DONE!!! 🌞\n", ITAL, BOLD, PURPLE)
 
-# general overview
-#C 4,10,-70 -0.2,0,0.7 90
-
-# z front
-#C 0,0,0 0,0,1 55
-# z back
-#C 0,0,0 0,0,-1 55
-
-# x front
-#C 0,0,0 1,0,0 55
-
-# x back
-#C 0,0,0 -1,0,0 55
-
-# y up
-#C 0,0,0 0,1,0 55
-# y down
-#C 0,0,0 0,-1,0 55
-
-# camera for testing
