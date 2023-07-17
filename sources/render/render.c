92967da1e783d24e2466370c4589049476ba7044
Luca Fischer <lucafischer@fastmail.com>
2023-07-17 18:15:32 +0200

    Remove comments

--- sources/render/render.c
+++ sources/render/render.c
@@ -3,45 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   render.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mvomiero <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: lde-ross <lde-ross@student.42berlin.de     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 15:37:24 by lde-ross          #+#    #+#             */
-/*   Updated: 2023/07/17 11:22:42 by mvomiero         ###   ########.fr       */
+/*   Updated: 2023/07/17 18:15:13 by lde-ross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minirt.h"
-
-/*static void	render_pixel(t_data *data)
-{
-	int			x;
-	int			y;
-	t_coord		pixel;
-	static int	i = 0;
-	t_ray		ray;
-
-	matrix_look_at(data);
-	y = -1;
-	while (++y < HEIGHT)
-	{
-		x = -1;
-		while (++x < WIDTH)
-		{
-			pixel.x = x;
-			pixel.y = y;
-			ray = get_ray(data, x, y);
-			data->pix.dir = ray.direction;
-			ray_tracer(data, pixel);
-			shade(data, pixel);
-		}
-		i++;
-		if (i == 10)
-		{
-			i = 0;
-			mlx_put_image_to_window(data->mlx, data->win, data->img, 0, 0);
-		}
-	}
-}*/
 
 void	render_pixel_loop(t_data *data)
 {
@@ -72,12 +41,6 @@ void	render_pixel_loop(t_data *data)
 	}
 }
 
-void	render_pixel(t_data *data)
-{
-	matrix_look_at(data);
-	render_pixel_loop(data);
-}
-
 void	render(t_data *data)
 {
 	int	pixel_bits;
@@ -88,6 +51,7 @@ void	render(t_data *data)
 		mlx_destroy_image(data->mlx, data->img);
 	data->img = mlx_new_image(data->mlx, WIDTH, HEIGHT);
 	data->buf = mlx_get_data_addr(data->img, &pixel_bits, &line_bytes, &endian);
-	render_pixel(data);
+	matrix_look_at(data);
+	render_pixel_loop(data);
 	mlx_put_image_to_window(data->mlx, data->win, data->img, 0, 0);
 }
